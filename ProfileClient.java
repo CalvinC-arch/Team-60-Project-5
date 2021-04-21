9a09981b3d6e4a363703b60249be3cd92f9c4a06
CalvinC-arch <62853291+CalvinC-arch@users.noreply.github.com>
2021-04-21 14:30:32 -0500

    Update ProfileClient.java

--- ProfileClient.java
+++ ProfileClient.java
@@ -7,27 +7,26 @@ import java.net.Socket;
 
 public class ProfileClient {
     public static void main(String[] args) throws IOException {
-        String hostName = JOptionPane.showInputDialog(null, "Enter the host name", "CampsGram",
-                JOptionPane.QUESTION_MESSAGE);
+        boolean check = false;
 
-        int i = 0;
-        String portNum;
-        do {
-            portNum = JOptionPane.showInputDialog(null, "Enter the port number", "CampsGram",
-                    JOptionPane.QUESTION_MESSAGE);
-            if (!(portNum.matches("\\d+"))) {
-                JOptionPane.showMessageDialog(null, "Enter a valid port number", "CampsGram",
-                        JOptionPane.ERROR_MESSAGE);
-            } else {
-                i++;
+        try { //connect to server and set up network io, display error message if connection unsuccessful
+            while(!check) {
+                Socket socket = new Socket("localhost", 1234);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+                check = true;
             }
-        } while (i == 0);
 
-        Socket socket = new Socket(hostName, Integer.parseInt(portNum));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: Connection Cannot be Established",
+                    "Campsgram", JOptionPane.ERROR_MESSAGE);
+        }
 
+        //Confirm connection successful
         JOptionPane.showMessageDialog(null, "Connection Established", "CampsGram",
                 JOptionPane.INFORMATION_MESSAGE);
+
     }
 }
