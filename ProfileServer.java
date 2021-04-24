16a75fcac0c1ec626e04bb897019128b1305fbaf
CalvinC-arch <62853291+CalvinC-arch@users.noreply.github.com>
2021-04-24 14:42:23 -0400

    Update ProfileServer.java

--- ProfileServer.java
+++ ProfileServer.java
@@ -1,25 +1,41 @@
 import java.io.*;
 import java.net.*;
 
-// Server class
+/**
+ * ProfileServer
+ *
+ * This class consists of a main method that manages server connections. The main logic loops constantly to detect new
+ * client connections. When a client connects, this class creates a new ProfileClientHandler object that is then used
+ * to interface with the client.
+ *
+ * Geeksforgeeks was referenced to determine how to utilize threads for mutliple client-server connections
+ *
+ * @author Team 60, Section 11
+ * @version 5/03/2021
+ *
+ */
+
+//Class uses port 1234
 public class ProfileServer
 {
     public static void main(String[] args) throws IOException
     {
 
-        ServerSocket ss = new ServerSocket(1234);
+        ServerSocket ss = new ServerSocket(1234); //server socket that the server uses to connect to clients
 
-        while (true)
+        while (true) //loops indefinitely so that the server is always responsive to new connections
         {
-            Socket s = null;
+            Socket s = null; //socket used by the client
 
             try
             {
 
                 s = ss.accept();
 
+                //confirm a client is connected, and outputs the socket information
                 System.out.println("A new client is connected : " + s);
 
+                //Create new input and output stream to communicate with client
                 DataInputStream dis = new DataInputStream(s.getInputStream());
                 DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 
@@ -28,9 +44,8 @@ public class ProfileServer
                 t.start();
 
             }
-            catch (Exception e){
+            catch (Exception e){ //close socket if an exception occurs
                 s.close();
-                e.printStackTrace();
             }
         }
     }
