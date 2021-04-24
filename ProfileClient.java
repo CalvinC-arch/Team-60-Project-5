2518f44e8603c5c91cef7d7c4611f79c78ac47f2
CalvinC-arch <62853291+CalvinC-arch@users.noreply.github.com>
2021-04-24 14:41:23 -0400

    Update ProfileClient.java

--- ProfileClient.java
+++ ProfileClient.java
@@ -5,9 +5,23 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * ProfileClient
+ *
+ * The .java file that the a user will run to connect to the server and access the social media functionality. It
+ * contains a main method that automatically connects the user to the server.
+ *
+ * N/A
+ *
+ * @author Team 60, Section 11
+ * @version 5/03/2021
+ *
+ */
+
 public class ProfileClient {
     public static void main(String[] args) throws IOException {
-        boolean check = false;
+
+        boolean check = false; //used to determine whether a connection to the server has been achieved
 
         try { //connect to server and set up network io, display error message if connection unsuccessful
             while(!check) {
@@ -19,7 +33,7 @@ public class ProfileClient {
                 check = true;
             }
 
-        } catch (IOException e) {
+        } catch (IOException e) { //display error message is connection not established
             JOptionPane.showMessageDialog(null, "Error: Connection Cannot be Established",
                     "Campsgram", JOptionPane.ERROR_MESSAGE);
         }
