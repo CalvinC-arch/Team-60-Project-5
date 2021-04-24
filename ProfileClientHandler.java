177464df8c7c02a4a6c191830a78e69d21a9acc0
CalvinC-arch <62853291+CalvinC-arch@users.noreply.github.com>
2021-04-24 14:41:53 -0400

    Update ProfileClientHandler.java

--- ProfileClientHandler.java
+++ ProfileClientHandler.java
@@ -2,18 +2,44 @@ import javax.swing.*;
 import java.io.*;
 import java.net.*;
 
+/**
+ * ProfileClientHandler
+ *
+ * This class contains the server side processing and user interface logic for the project. It is created from a thread
+ * created by the ProfileServer class. The constructor allows multiple objects of this type to be created, allowing for
+ * multiple clients to connect to the server
+ *
+ * Geeksforgeeks was referenced to determine how to utilize threads for mutliple client-server connections
+ *
+ * @author Team 60, Section 11
+ * @version 5/03/2021
+ *
+ */
+
 class ProfileClientHandler extends Thread {
 
-    final DataInputStream dis;
-    final DataOutputStream dos;
-    final Socket s;
+    final DataInputStream dis; //data input stream used to communicate with the server
+    final DataOutputStream dos; //data output stream used to communicate with the server
+    final Socket s; //socket used to communicate with the server
 
-    // Constructor
+    /**
+     * Creates a new ProfileClientHandler object
+     *
+     * @param s data input stream used to communicate with the server
+     * @param dis data output stream used to communicate with the server
+     * @param dos socket used to communicate with the server
+     */
+
     public ProfileClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) {
         this.s = s;
         this.dis = dis;
         this.dos = dos;
     }
+
+    /**
+     * Runs functionality for the server
+     *
+     */
 
     @Override
     public void run() {
