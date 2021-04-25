dcaa475f77c45ac7cecb93205ac7ac754f292b45
CalvinC-arch <62853291+CalvinC-arch@users.noreply.github.com>
2021-04-25 16:55:06 -0400

    Add files via upload

--- Profile.java
+++ Profile.java
@@ -1,7 +1,10 @@
-import java.io.*;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.*;
+import java.util.Locale;
 
 public class Profile {
+
     private String username;
     private ArrayList<String> interests;
     private ArrayList<String> friends;
@@ -20,6 +23,99 @@ public class Profile {
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.aboutMe = aboutMe;
+    }
+
+    public Profile(String filename) throws IOException {
+
+        String profile = null; //line of profile info from .csv
+        String[] profileInfo; //array of profile info from .csv
+        int listLength; //length interests and friends arrays
+        int index = 0; //index of profileInfo
+        int tempIndex; //holds index that is reached before appending to a list
+
+        this.interests = new ArrayList<>();
+        this.friends = new ArrayList<>();
+
+        try {
+            File f = new File(filename);
+            FileReader fr = new FileReader(f);
+            BufferedReader bfr = new BufferedReader(fr);
+
+            profile = bfr.readLine();
+
+            bfr.close();
+        } catch (IOException e) {
+            //DISPLAY ERROR GUI
+            e.printStackTrace();
+        }
+
+        profileInfo = profile.split(",");
+
+        this.username = profileInfo[index];
+        index++;
+
+        listLength = Integer.parseInt(profileInfo[index]);
+        index++;
+
+        tempIndex = index;
+
+        while (index < listLength + tempIndex) {
+            interests.add(profileInfo[index]);
+            index++;
+        }
+
+        listLength = Integer.parseInt(profileInfo[index]);
+        index++;
+
+        tempIndex = index;
+
+        while (index < listLength + tempIndex) {
+            friends.add(profileInfo[index]);
+            index++;
+        }
+
+        this.education = profileInfo[index];
+        index++;
+
+        this.email = profileInfo[index];
+        index++;
+
+        this.phoneNumber = Long.parseLong(profileInfo[index]);
+        index++;
+
+        this.aboutMe = profileInfo[index] + ", ";
+        index++;
+
+        while (index < profileInfo.length) {
+            this.aboutMe = this.aboutMe + profileInfo[index] + ", ";
+            index++;
+        }
+
+        this.aboutMe = this.aboutMe.substring(0, this.aboutMe.length() - 2);
+    }
+
+    public void writeExportFile() throws IOException {
+
+        //creating new file and printwriter for this profile
+        File f = new File(this.username + "Export.csv");
+        PrintWriter pw = new PrintWriter(new FileWriter(f, false));
+        pw.print(this.username + ',' + interests.size() + ',');
+
+        //loop to write each element of interests list to file
+        for (int i = 0; i < interests.size(); i++) {
+            pw.print(this.interests.get(i) + ',');
+        }
+
+        pw.print(this.friends.size());
+        pw.print(",");
+
+        //loop to write each element of friends list to file
+        for (int x = 0; x < friends.size(); x++) {
+            pw.print(friends.get(x) + ',');
+        }
+
+        pw.print(this.education + ',' + this.email + ',' + this.phoneNumber + ',' + this.aboutMe);
+        pw.close();
     }
 
     public String getUsername() {
@@ -78,21 +174,4 @@ public class Profile {
         this.username = username;
     }
 
-    public void writeExportFile() throws IOException {
-        //creating new file for this profile
-        File f = new File(this.username + "Export.csv");
-        PrintWriter pw = new PrintWriter(new FileWriter(f, false));
-        pw.print(this.username + ',' + interests.size() + ',');
-        //loop to write each element of interests list to file
-        for (int i = 0; i < interests.size(); i++) {
-            pw.print(this.interests.get(i) + ',');
-        }
-        pw.print(this.friends.size() + ',');
-        //loop to write each element of friends list to file
-        for (int x = 0; x < friends.size(); x++) {
-            pw.print(friends.get(x) + ',');
-        }
-        pw.print(this.education + ',' + this.email + ',' + this.phoneNumber + ',' + this.aboutMe);
-        pw.close();
-    }
 }
