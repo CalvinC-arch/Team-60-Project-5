5c568661ff52f9b0cbb752a44daf19350ecb8327
Eric Schaeflein <ems1207@comcast.net>
2021-04-30 21:41:58 -0400

    poopy poopy butt

--- ProfileTester.java
+++ ProfileTester.java
@@ -12,6 +12,7 @@ public class ProfileTester {
         friends.add("Joe");
         friends.add("Eric");
         friends.add("Naia");
+        friends.add("girvinn");
         Profile jeff = new Profile("chen3801", interests, friends, "Purdue University",
                 "chen3801@purdue.edu", 5167341551L, "hello, I am Jeff");
         jeff.writeExportFile();
